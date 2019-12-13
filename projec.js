77bd84b5e3779790c954f818f4440e31e9ad690a
180107242 <55230789+180107242@users.noreply.github.com>
2019-12-13 06:26:48 +0600

    Add files via upload

--- projec.js
+++ projec.js
@@ -1,6 +1,6 @@
 var i = 0;       
 var images = [];  
-var time = 1000;  
+var time = 2000;  
 
 images[0] = "111.jpg";
 images[1] = "222.jpg";
@@ -14,6 +14,11 @@ images[7] = "888.jpg";
 
 function changeImg(){
   document.slide.src = images[i];
+  document.slide1.src = images[i];
+  document.slide2.src = images[i];
+  document.slide3.src = images[i];
+
+
 
   if(i < images.length - 1){
     i++; 
@@ -24,3 +29,4 @@ function changeImg(){
   setTimeout("changeImg()", time);
 }
 window.onload=changeImg;
+
