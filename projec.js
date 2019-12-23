3b0091290086cc01de48725200d13a077886155d
180107242 <55230789+180107242@users.noreply.github.com>
2019-12-23 22:46:19 +0600

    Add files via upload

--- projec.js
+++ projec.js
@@ -10,21 +10,29 @@ images[4] = "555.jfif";
 images[5] = "666.svg";
 images[6] = "777.jfif";
 images[7] = "888.jpg";
+images[8] = "1.jpg";
 
 
 function changeImg(){
-  document.slide.src = images[i];
-  document.slide1.src = images[i];
-  document.slide2.src = images[i];
-  document.slide3.src = images[i];
-
-
-
-  if(i < images.length - 1){
-    i++; 
-  } else { 
-    i = 0;
+  // document.slide.src = images[i];
+  // document.slide1.src = images[i+1];
+  // document.slide2.src = images[i+2];
+  // document.slide3.src = images[i+3];
+  if (i <= 5) {
+	  document.slide.src = images[i];
+	  document.slide1.src = images[i+1];
+	  document.slide2.src = images[i+2];
+	  document.slide3.src = images[i+3];
+  } else {
+  	i = 0;
   }
+  ++i;
+
+
+  // if(i < images.length - 1){
+  //   i++; 
+  // } else { 
+  // }
 
   setTimeout("changeImg()", time);
 }
